3c5e8b96da71c495f7ac8eca74bc497f2268302b
Hank Carter <carterh@gatech.edu>
2020-08-30 17:02:43 -0400

    Template file structure and adding some basic functionality to the core diff program

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # urlDiffAlert
 A basic email alert when a webpage changes.
+
+Useful resources:
+* https://docs.python.org/3/library/difflib.html
+* https://3.python-requests.org/
+* https://docs.python.org/3/library/pickle.html
---
+++ lib/url_diff.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import requests
+import pickle
+
+default_cache = './temp/url_cache.bin'
+
+class url_cache:
+    #Request timeout in seconds
+    timeout = 1
+    def __init__(self):
+        self.cache = {}
+
+    #Takes a url and optional regex.  If the diff of the cached version with the latest version contains regex, return the HTML diff.  Otherwise return None.
+    def url_diff(self, url, regex=''):
+        try: 
+            current_page = requests.get(url, timeout=url_cache.timeout)
+            loaded_page = requests.get(url).text
+            if current_page.text != loaded_page:
+                return 'There was a diff'
+            else:
+                return None
+        except:
+            print('An exception occurred')
+            return None
+
+def main():
+    #load cache
+    try:
+        cache_file = open(default_cache, 'rb')
+        cache = pickle.load(cache_file)
+        cache_file.close()
+    except:
+        cache = url_cache()
+
+    while True:
+        #query url
+        url = input('Enter a url. Enter \'quit\' to exit: ')
+        if url == 'quit':
+            break
+
+        #print results
+        results = cache.url_diff(url)
+        print(results)
+        
+        #update the cache
+        cache.cache[url] = results
+
+    #write the cache
+    cache_file = open(default_cache, 'wb')
+    pickle.dump(cache,cache_file)
+    cache_file.close()
+
+if __name__ == '__main__':
+    main()

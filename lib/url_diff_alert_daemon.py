4c83944cef6196310d115ef11bc4266be6225b24
Hank Carter <carterh@gatech.edu>
2020-09-02 22:51:47 -0400

    Outlined daemon and added json-formatted config file

---
+++ lib/url_diff_alert_daemon.py
@@ -0,0 +1,19 @@
+import url_diff
+import json
+
+def main():
+    #Load state
+
+    #Load config
+
+    #Create new state entries for config entries that don't already exist (or if the regex is different)
+
+    #while true:
+    #Every X minutes, run url_diff for every key in the state dictionary
+    #If no change, continue
+    #Else:
+    #send email alert, update state, write the cache to disk
+    pass
+
+if __name__ == '__main__':
+    main()

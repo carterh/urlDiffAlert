46d7716a421b0477b469d9ce788ce5a16bc36d6c
Hank Carter <carterh@gatech.edu>
2020-09-28 22:00:21 -0400

    Wrote daemon. Still need to write diff code to print the html diff for the page

--- lib/url_diff.py
+++ lib/url_diff.py
@@ -1,58 +1,29 @@
 #!/usr/bin/env python3
 
 import requests
-import pickle
-from email_alert import send_alert
 
-default_cache = './temp/url_cache.bin'
-
-class url_cache:
-    #Request timeout in seconds
-    timeout = 1
-    def __init__(self):
-        self.cache = {}
-
-    #Takes a url and optional regex.  If the diff of the cached version with the latest version contains regex, return the HTML diff.  Otherwise return None.
-    def url_diff(self, url, regex=''):
-        try: 
-            current_page = requests.get(url, timeout=url_cache.timeout)
-            loaded_page = requests.get(url).text
-            if current_page.text != loaded_page:
-                return 'There was a diff'
-            else:
-                return None
-        except:
-            print('An exception occurred')
+#Takes a url and optional regex.  If the diff of the cached version with the latest version contains regex, return the HTML diff.  Otherwise return None.
+def url_diff(url, regex, cache_page):
+    try: 
+        loaded_page = requests.get(url, timeout=1).text
+        if cache_page != loaded_page:
+            return 'There was a diff for url: ' + url
+        else:
             return None
+    except BaseException as e:
+        print(str(e))
+        return None
 
 def main():
-    #load cache
-    try:
-        cache_file = open(default_cache, 'rb')
-        cache = pickle.load(cache_file)
-        cache_file.close()
-    except:
-        cache = url_cache()
-
     while True:
         #query url
         url = input('Enter a url. Enter \'quit\' to exit: ')
         if url == 'quit':
             break
 
-        #email and print results
-        results = cache.url_diff(url)
+        cache_page = requests.get(url, timeout=1).text
+        results = url_diff(url, '', cache_page)
         print(results)
-        send_alert(results, ['ratherBrehearsin@yahoo.com'])
-
-        
-        #update the cache
-        cache.cache[url] = results
-
-    #write the cache
-    cache_file = open(default_cache, 'wb')
-    pickle.dump(cache,cache_file)
-    cache_file.close()
 
 if __name__ == '__main__':
     main()
--- lib/url_diff_alert_daemon.py
+++ lib/url_diff_alert_daemon.py
@@ -1,19 +1,74 @@
-import url_diff
+#!/usr/bin/env python3
+
+from url_diff import url_diff
 import json
+import pickle
+from collections import defaultdict
+from time import sleep
+from email_alert import send_alert
+
+class config:
+    def __init__(self, rules, recipients):
+        self.rules = rules
+        self.recipients = recipients
+        self.check_interval = 5*60
+
+    @staticmethod
+    def load_config(path):
+        config_file = open(path)
+        config_map = json.load(config_file)
+        config_file.close()
+        return config(config_map['rules'], config_map['recipients'])
+
+default_cache = './temp/url_cache.bin'
+default_config = './config.json'
+
+class url_cache:
+    def __init__(self):
+        self.cache = defaultdict(str)
+
+class daemon:
+    def __init__(self, config, state):
+        self.config = config
+        self.state = state
+
+    def start(self):
+        print('Starting daemon')
+        while True:
+            for rule in self.config.rules:
+                url = rule['url']
+                regex = rule['regex']
+                try:
+                    result = url_diff(rule['url'], rule['regex'], self.state.cache[(url,regex)])
+                    if result:
+                        print('New result for ' + url + ' , emailing')
+                        send_alert(result, ['ratherBrehearsin@yahoo.com'])
+                        self.state.cache[(url,regex)] = result
+                except BaseException as e:
+                    print(str(e))
+            
+            #write the cache, sleep
+            print('Writing cache')
+            try:
+                cache_file = open(default_cache, 'wb')
+                pickle.dump(self.state,cache_file)
+                cache_file.close()
+            except BaseException as e:
+                print(str(e))
+            sleep(self.config.check_interval)
 
 def main():
-    #Load state
+    conf = config.load_config(default_config) 
 
-    #Load config
+    try:
+        cache_file = open(default_cache, 'rb')
+        cache = pickle.load(cache_file)
+        cache_file.close()
+    except:
+        cache = url_cache()
 
-    #Create new state entries for config entries that don't already exist (or if the regex is different)
-
-    #while true:
-    #Every X minutes, run url_diff for every key in the state dictionary
-    #If no change, continue
-    #Else:
-    #send email alert, update state, write the cache to disk
-    pass
+    proc = daemon(conf, cache)
+    proc.start()
 
 if __name__ == '__main__':
     main()
